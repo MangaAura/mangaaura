5f1bf1ee844ef95fde6b3245e6fac6462ae32e04
Miguel <miguelgallegovivanco@gmail.com>
2026-05-27 01:40:10 +0200

    fix: remove unused useRouter import in MangaCard (build fix)

--- src/components/Creator/MangaCard.tsx
+++ src/components/Creator/MangaCard.tsx
@@ -9,7 +9,6 @@ import {
 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 import { Button } from '@/components/ui/Button';
